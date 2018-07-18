d820d68390a20e5fa79f8d0894aa0402062d522f
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-07-18 03:08:40 -0400

    airline 3

--- airline3/airline3/wsgi.py
+++ airline3/airline3/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for airline4 project.
+WSGI config for airline3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airline4.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airline3.settings")
 
 application = get_wsgi_application()
